8a1a8d54a07316d8c90e69119503844b7ea0405c
SAI MIHIR NATH NAYAKANTI <33085599+the-darklord@users.noreply.github.com>
2022-09-01 10:09:56 +0530

    Delete LAB directory

--- LAB/Q1/Q1A/q1a.v
+++
@@ -1,5 +0,0 @@
-module q1a(A,B,C,D,f);
-input A,B,C,D;
-output f;
-assign f=(B&(~C)&(~D))|(~A&B)|(C&D)|(~B&C);
-endmodule
--- LAB/Q1/Q1B/q1b.v
+++
@@ -1,5 +0,0 @@
-module q1b(A,B,C,D,f);
-input A,B,C,D;
-output f;
-assign f = (~B&C)|(~B&D)|(B&~C&~D);
-endmodule
