dccfe4f6a834862f5acf508d9907e8dabd5b3e34
the-darklord <saimihirnath.nayakanti1@gmail.com>
2022-09-18 17:40:58 +0530

    LAB4

--- LAB-4/Q2/qb_tb.v
+++ LAB-4/Q2/qb_tb.v
@@ -16,8 +16,8 @@ $dumpvars(0, qb_tb);
 carryin=1'b0;x=4'b1000;y=4'b0001;#20;
 carryin=1'b1;x=4'b1000;y=4'b0001;#20;
 
-carryin=1'b0;x=4'b1001;y=4'b0011;#20;
-carryin=1'b1;x=4'b1001;y=4'b0011;#20;
+/*carryin=1'b0;x=4'b1001;y=4'b0011;#20;
+carryin=1'b1;x=4'b1001;y=4'b0011;#20;*/
 
 $display("Test complete");
 end
