adc28c1bd6e82c922743eca77c4bcc397b51c3a1
the-darklord <saimihirnath.nayakanti1@gmail.com>
2022-10-20 10:42:11 +0530

    LAB7

--- LAB-7/Q3/q_tb.v
+++ LAB-7/Q3/q_tb.v
@@ -1,17 +1,17 @@
 `timescale 1ns/1ns
 `include "q.v"
 module q_tb();
-reg [3:0]i;
-wire f;
-q f1(i,f);
+reg [3:0]b;
+wire [3:0]e;
+q f1(b,e);
 initial
 begin
 $dumpfile("q_tb.vcd");
 $dumpvars(0,q_tb);
 
-i=4'b1110; #20;
-i=4'b1010; #20;
-i=4'b0010; #20;
+b=4'b1000; #20;
+b=4'b1001; #20;
+b=4'b0010; #20;
 
 $display("Test Complete");
 end
